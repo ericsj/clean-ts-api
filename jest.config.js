071871d695e5694463425c3d0df0616c41c56287
Eric Segala Jovelli <ericsegalajovelli@gmail.com>
2022-05-13 14:50:59 -0300

    feat: ensure SignUpController calls addAccount with right values

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,10 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
-  testEnvironment: "jest-environment-node",
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'jest-environment-node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
-};
+}
