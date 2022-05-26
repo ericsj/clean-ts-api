28bb8c473079162047ccc088ba1065c4bdb25d2c
Eric Segala Jovelli <ericsegalajovelli@gmail.com>
2022-05-26 10:15:16 -0300

    feat: ensure LoginController returns 200 if valid credentials are provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -15,18 +15,18 @@ export class LoginController implements Controller {
     try {
       const requiredFields = ['email', 'password']
       for (const field of requiredFields) {
-        if (!httpRequest.body[field]) { return new Promise(resolve => resolve(badRequest(new MissingParamError(field)))) }
+        if (!httpRequest.body[field]) { return badRequest(new MissingParamError(field)) }
       }
       const { email, password } = httpRequest.body
       const isValid = this.emailValidator.isValid(email)
       if (!isValid) {
-        return new Promise(resolve => resolve(badRequest(new InvalidParamError('email'))))
+        return badRequest(new InvalidParamError('email'))
       }
       const accessToken = await this.authentication.auth(email, password)
       if (!accessToken) {
         return unauthorized()
       }
-      return new Promise(resolve => resolve(ok('')))
+      return ok({ accessToken: 'any_token' })
     } catch (error) {
       return serverError(error)
     }
