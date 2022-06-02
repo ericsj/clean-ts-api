67e9ef6d2ba12d848bf611442cf630a984127849
Eric Segala Jovelli <ericsegalajovelli@gmail.com>
2022-06-02 12:30:50 -0300

    feat: ensure AddSurveyController returns 500 if AddSurvey throws

--- src/presentation/controllers/survey/add-survey/add-survey-controller-protocols.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller-protocols.ts
@@ -1,3 +1,4 @@
 export * from '../../../protocols'
 export * from '../../../protocols/validation'
 export * from '../../../../domain/usecases/add-survey'
+export * from '../../../helpers/http/http-helper'
